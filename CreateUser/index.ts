784862b4dcb222f53060ca38df875b1bfbf87379
Steve Drake <sdrake@truenorthit.co.uk>
2022-04-22 11:51:59 +0100

    code

---
+++ CreateUser/index.ts
@@ -0,0 +1,23 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable orchestration function
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *   function app in Kudu
+ */
+
+import { AzureFunction, Context } from "@azure/functions"
+
+const activityFunction: AzureFunction = async function (context: Context): Promise<string> {
+    context.log('Assigning User Roles');
+
+    if ( Math.random() * 2 < 1 ) {
+        throw 'error with roles';
+    } 
+    return 'Roles Assigned';
+};
+
+export default activityFunction;
---
+++ DurableFunctionsOrchestratorJS1/index.ts
@@ -0,0 +1,25 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an HTTP starter function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable activity function (default name is "Hello")
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your 
+ *    function app in Kudu
+ */
+
+import * as df from "durable-functions"
+
+const orchestrator = df.orchestrator(function* (context) {
+    const outputs = [];
+
+    const retryOptions =  new df.RetryOptions(1000, 100);
+
+    outputs.push(yield context.df.callActivityWithRetry("CreateUser", retryOptions,"UserID" ));
+    outputs.push(yield context.df.callActivityWithRetry("LinkUserToOrg", retryOptions,"UserID" ));
+    outputs.push(yield context.df.callActivityWithRetry("AssignUserRoles", retryOptions,"UserID " ));
+    return outputs;
+});
+
+export default orchestrator;
